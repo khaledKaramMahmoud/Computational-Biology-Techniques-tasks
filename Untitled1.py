800c88b4aacdc5c17930ad67464ac53d0915ded8
khaledKaramMahmoud <87311403+khaledKaramMahmoud@users.noreply.github.com>
2021-12-17 23:31:36 +0200

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-# خالد كرم محمود يوسف
-from urllib.request import urlretrieve
 from pyopenms.Constants import *
-import pyopenms
-from pyopenms import ElementDB, EmpiricalFormula, CoarseIsotopePatternGenerator, FineIsotopePatternGenerator, ResidueDB,     ModificationsDB, RibonucleotideDB, AASequence, Residue, FASTAEntry, FASTAFile
-import math
-from matplotlib import pyplot as plt
-
 help(pyopenms.Constants)
 print("Avogadro's number :", pyopenms.Constants.AVOGADRO)
 ########################################################
 # elements
-
 DataBase = ElementDB()
-
 DataBase.hasElement("H")
 DataBase.hasElement("He")
-
 hydrogen = DataBase.getElement("H")
 print(hydrogen.getName())
 print(hydrogen.getSymbol())
@@ -34,29 +18,24 @@ print(heleim.getSymbol())
 print(heleim.getMonoWeight())
 print(heleim.getAverageWeight())
 isotopes = heleim.getIsotopeDistribution()
-
 print("One mole of hydrogen weighs equals =", 2 * hydrogen.getAverageWeight(), "grams")
 print("One mole of 16O2 weighs equals=", 2 * hydrogen.getMonoWeight(), "grams")
 ########################################################
 # isotops
 hydrogn_isoDist = {"mass": [], "abundance": []}
 heliem_isoDist = {"mass": [], "abundance": []}
-
 hydrogen = DataBase.getElement("H")
 isotopes = hydrogen.getIsotopeDistribution()
 for iso in isotopes.getContainer():
     print("hydrogen isotope:", iso.getMZ(), "has abundance", iso.getIntensity() * 100, "%")
     hydrogn_isoDist["mass"].append(iso.getMZ())
     hydrogn_isoDist["abundance"].append((iso.getIntensity() * 100))
-
 heleim = DataBase.getElement("He")
 isotopes = heleim.getIsotopeDistribution()
 for iso in isotopes.getContainer():
     print("heleim isotope", iso.getMZ(), "has abundance", iso.getIntensity() * 100, "%")
     heliem_isoDist["mass"].append(iso.getMZ())
     heliem_isoDist["abundance"].append((iso.getIntensity() * 100))
-
-
 def adjustText(x1, y1, x2, y2):
     if y1 > y2:
         plt.annotate('%0.3f' % (y2), xy=(x2, y2), xytext=(x2 + 0.5, y2 + 9),
@@ -68,14 +47,13 @@ def adjustText(x1, y1, x2, y2):
                      textcoords='data',
                      arrowprops=dict(arrowstyle="->", color='r', lw=0.5),
                      horizontalalignment='right', verticalalignment='top')
-
-
 ########################################################
 def plotDistribution(distribution):
     n = len(distribution["mass"])
     for i in range(0, n):
         plt.vlines(x=distribution["mass"][i], ymin=0, ymax=distribution["abundance"][i])
-        if int(distribution["mass"][i - 1]) == int(distribution["mass"][i])                 and i != 0:
+        if int(distribution["mass"][i - 1]) == int(distribution["mass"][i]) \
+                and i != 0:
             adjustText(distribution["mass"][i - 1], distribution["abundance"][i - 1],
                        distribution["mass"][i], distribution["abundance"][i])
         else:
@@ -86,11 +64,14 @@ def plotDistribution(distribution):
     plt.ylim([0, 110])
     plt.xticks(range(math.ceil(distribution["mass"][0]) - 2,
                      math.ceil(distribution["mass"][-1]) + 2))
-
-
 ########################################################
+from urllib.request import urlretrieve
+import pyopenms
+from pyopenms import ElementDB, EmpiricalFormula, CoarseIsotopePatternGenerator, FineIsotopePatternGenerator, ResidueDB, \
+    ModificationsDB, RibonucleotideDB, AASequence, Residue, FASTAEntry, FASTAFile
+import math
+from matplotlib import pyplot as plt
 plt.figure(figsize=(10, 7))
-
 plt.subplot(1, 2, 1)
 plt.title("Isotopic distribution of Hydrogen")
 plotDistribution(hydrogn_isoDist)
@@ -363,9 +344,7 @@ for i in l:
         subVakaMZ+=iso.getMZ()
 print(subVakaMZ)
 #Task3
-
 fh = open("gen.txt")
-
 dig = ProteaseDigestion()
 dig.setEnzyme('Lys-C')
 bsa = "".join([l.strip() for l in fh.readlines()[1:]])
@@ -374,4 +353,112 @@ result = []
 dig.digest(bsa, result)
 print(result[4].toString())
 len(result)
+##############################################################
+#Spectrum alignment
+from urllib.request import urlretrieve
+gh = "https://raw.githubusercontent.com/OpenMS/pyopenms-extra/master"
+urlretrieve (gh + "/src/data/YIC(Carbamidomethyl)DNQDTISSK.mzML", "obs.mzML")
+exp = MSExperiment()
+MzMLFile().load("obs.mzML", exp)
+spectra = exp.getSpectra()
+obsSpectrum = spectra[0]
+#################################################
+#theoretical spectrum
+tsg = TheoreticalSpectrumGenerator()
+theo_spectrum = MSSpectrum()
+p = tsg.getParameters()
+p.setValue("add_y_ions", "true")
+p.setValue("add_b_ions", "true")
+p.setValue("add_metainfo", "true")
+tsg.setParameters(p)
+peptide = AASequence.fromString("YIC(Carbamidomethyl)DNQDTISSK")
+tsg.getSpectrum(theo_spectrum, peptide, 1, 2)
+#########################################################
+#observed and theoretical spectrum
+import numpy as np
+from matplotlib import pyplot as plt
+def mirror_plot(obs_mz, obs_int, theo_mz, theo_int, title):
+    obs_int = [element / max(obs_int) for element in obs_int] 
+    theo_int = [element * -1 for element in theo_int] 
+    plt.figure(figsize=(12,8))
+    plt.bar(obs_mz, obs_int, width = 3.0)
+    plt.bar(theo_mz, theo_int, width = 3.0)
+    plt.title(title)
+    plt.ylabel('intensity')
+    plt.xlabel('m/z')
+    
+obs_mz, obs_int = observed_spectrum.get_peaks()
+print(min(obs_mz))
+print(max(obs_mz))
+theo_mz, theo_int = [], []
+for mz, intensity in zip(*theo_spectrum.get_peaks()):
+    if mz >= 200.0 and mz <= 800.0:
+        theo_mz.append(mz)
+        theo_int.append(intensity)
 
+title = 'Observed vs theoretical spectrum'
+################################################
+#"Mirror Plot"
+mirror_plot(obs_mz, obs_int, theo_mz, theo_int, title)
+print("Number of matched peaks: " + str(len(alignment)))
+print("ion\ttheo. m/z\tobserved m/z")
+for theo_idx, obs_idx in alignment:
+    ion_name = theo_spectrum.getStringDataArrays()[0][theo_idx].decode()
+    ion_charge = theo_spectrum.getIntegerDataArrays()[0][theo_idx]
+    print(ion_name + "\t" + str(ion_charge) + "\t"
+      + str(theo_spectrum[theo_idx].getMZ())
+      + "\t" + str(observed_spectrum[obs_idx].getMZ()))
+ ##########################################
+#"Spec fargement Gen"
+tsg = TheoreticalSpectrumGenerator()
+spec1 = MSSpectrum()
+peptide = AASequence.fromString("DFPIANGER")
+p = Param()
+p.setValue("add_b_ions", "false")
+p.setValue("add_metainfo", "true")
+tsg.setParameters(p)
+tsg.getSpectrum(spec1, peptide, 1, 1) 
+print("Spectrum 1 of", peptide, "has", spec1.size(), "peaks.")
+for ion, peak in zip(spec1.getStringDataArrays()[0], spec1):
+    print(ion.decode(), "is generated at m/z", peak.getMZ())
+############################################   
+#Plot
+plt.bar(spec1.get_peaks()[0], spec1.get_peaks()[1], snap=False) 
+plt.xlabel("m/z")
+plt.ylabel("intensity")
+############################################
+#Ions "Names"
+mz,i = spec1.get_peaks()
+annot = spec1.getStringDataArrays()[0]
+bars = plt.bar(mz, i, snap=False)
+idx = 0
+for rect in bars:
+    height = rect.get_height()
+    plt.text(rect.get_x() + rect.get_width() / 2.0, height, annot[idx].decode(), ha='center', va='bottom', rotation=90)
+    idx += 1
+plt.ylim(top=1.2)
+plt.xlabel("m/z")
+plt.ylabel("intensity")
+#########################################
+#Full fragment ion spectrum
+spec2 = MSSpectrum()
+p.setValue("add_b_ions", "true")
+p.setValue("add_a_ions", "true")
+p.setValue("add_losses", "true")
+p.setValue("add_metainfo", "true")
+tsg.setParameters(p)
+tsg.getSpectrum(spec2, peptide, 1, 2)
+print("Spectrum 2 of", peptide, "has", spec2.size(), "peaks.")
+for ion, peak in zip(spec2.getStringDataArrays()[0], spec2):
+    print(ion.decode(), "is generated at m/z", peak.getMZ())
+
+exp = MSExperiment()
+exp.addSpectrum(spec1)
+exp.addSpectrum(spec2)
+MzMLFile().store("DFPIANGER.mzML", exp)
+############################################
+#Visualization
+import matplotlib.pyplot as plt
+plt.bar(spec2.get_peaks()[0], spec2.get_peaks()[1], snap=False) 
+plt.xlabel("m/z")
+plt.ylabel("intensity")
